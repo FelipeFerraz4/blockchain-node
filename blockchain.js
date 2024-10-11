4443fc214df6826ab08981010e3935ecca9023ca
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-10-11 11:00:09 -0300

    feat: created block mining

--- blockchain.js
+++ blockchain.js
@@ -9,12 +9,12 @@ class Transactions {
 } 
 
 class Block {
-    constructor(timestamp, lastHash, data, nonce) {
+    constructor(timestamp, lastHash, data) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = this.calculateBlockHash();
         this.data = data;
-        this.nonce = nonce;
+        this.nonce = 0;
     }
 
     calculateBlockHash() {
@@ -25,39 +25,62 @@ class Block {
 
     formatTimestamp() {
         const date = new Date(this.timestamp);
-        return date.toISOString(); // Formato ISO 8601
+        return date.toISOString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== '0'.repeat(difficulty)){
+            this.nonce++;
+            this.hash = this.calculateBlockHash();
+        }
     }
 }
 
 class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
+    constructor(Address) {
+        this.miningReward = 100.00;
+        this.chain = [this.createGenesisBlock(Address)];
         this.difficulty = 4;
         this.pendingTransactionPool = [];
-        this.miningReward = 100;
     }
 
-    createGenesisBlock() {
+    createGenesisBlock(miningRewardAddress) {
         const zeroHash = '0000000000000000000000000000000000000000000000000000000000000000';
-        return new Block(Date.now(), zeroHash, [], 0);
+        const genesisTrasactions = new Transactions('Origem', miningRewardAddress, this.miningReward)
+        return new Block(Date.now(), zeroHash, [genesisTrasactions], 0);
     }
 
     getLastBlock() {
         return this.chain[this.chain.length - 1]
     }
 
+    createTransaction(transaction) {
+        this.pendingTransactionPool.push(transaction);
+    }
+
+    minePendingTransactions(miningRewardAddress) {
+        this.pendingTransactionPool.push(new Transactions('Origem', miningRewardAddress, this.miningReward));
+
+        let block = new Block(Date.now(), this.getLastBlock().hash, this.pendingTransactionPool);
+        
+        block.mineBlock(this.difficulty);
+
+        this.chain.push(block);
+    }
+
     printBlockchain() {
         this.chain.forEach(block => {
-            console.log(`Block Nonce: ${block.nonce}\n`+
+            console.log(`Block\n` +
+            `Nonce: ${block.nonce}\n`+
             `Hash: ${block.hash}\n`+
             `Timestamp: ${block.formatTimestamp()}\n`+ 
             `Last Hash: ${block.lastHash}\n`+
             `Transactions:`);
     
             block.data.forEach(transaction => {
-                console.log(`From Address: ${transaction.fromAddress}`);
-                console.log(`To Address: ${transaction.toAddress}`);
-                console.log(`Value: ${transaction.value}`);
+                console.log(`   From Address: ${transaction.fromAddress}`);
+                console.log(`   To Address: ${transaction.toAddress}`);
+                console.log(`   Value: ${transaction.value}`);
             });
 
             console.log(`\n`)
@@ -65,5 +88,11 @@ class Blockchain {
     }
 }
 
-const bitcoin = new Blockchain();
+const bitcoin = new Blockchain('address1');
+
+bitcoin.createTransaction(new Transactions('address1', 'address2', 10));
+bitcoin.createTransaction(new Transactions('address1', 'address3', 10));
+
+
+bitcoin.minePendingTransactions('address2');
 bitcoin.printBlockchain();
