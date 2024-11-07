419e94ca4696a1838f250fa143ae7be75e7a5be2
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-11-07 08:30:51 -0300

    feat(transactions): created verify transactions

--- src/transactions.js
+++ src/transactions.js
@@ -13,6 +13,12 @@ class Transactions {
     sign.update(this.fromAddress + this.toAddress + this.value).end();
     this.signature = sign.sign(privateKey, "hex");
   }
+
+  verifyTransaction(publicKey) {
+    const verify = crypto.createVerify("SHA256");
+    verify.update(this.fromAddress + this.toAddress + this.value).end();
+    return verify.verify(publicKey, this.signature, "hex");
+  }
 }
 
 export default Transactions;
