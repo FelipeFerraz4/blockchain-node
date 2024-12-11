ece062f3e52b1a4692e4296cc084c14880b4cf03
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-12-10 23:50:25 -0300

    feat(blockchain.js): added balanceBook in the block class

--- src/block.js
+++ src/block.js
@@ -1,19 +1,25 @@
 import crypto from "crypto";
 
 class Block {
-  constructor(timestamp, lastHash, data) {
+  constructor(timestamp, lastHash, data, balanceBook) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = this.calculateBlockHash();
     this.data = data;
+    this.balanceBook = balanceBook;
     this.nonce = 0;
   }
 
   calculateBlockHash() {
+    const balanceBookString = JSON.stringify(Array.from(this.balanceBook.entries()));
     return crypto
       .createHash("sha256")
       .update(
-        this.timestamp + this.lastHash + JSON.stringify(this.data) + this.nonce
+        this.timestamp + 
+        this.lastHash + 
+        JSON.stringify(this.data) + 
+        this.nonce + 
+        balanceBookString
       )
       .digest("hex");
   }
