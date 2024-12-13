c8bc2da3b00a07f7d931f8d3976c1b4a6e0ff8df
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-12-12 22:47:54 -0300

    docs: add comments in keyPair class functions

--- src/keyPair.js
+++ src/keyPair.js
@@ -18,6 +18,7 @@ class KeyPair {
     this.address = this.generateAddressFromKey(publicKey);
   }
 
+  // Method to generate an address from the public key using hashing techniques
   generateAddressFromKey(publicKey) {
     const sha256Hash = crypto.createHash("sha256").update(publicKey).digest();
     const ripemd160 = crypto
@@ -30,27 +31,29 @@ class KeyPair {
     return prefix.concat(ripemd160);
   }
 
+  // Method to sign a transaction using the private key
   signTransaction(transaction) {
     const sign = crypto.createSign("SHA256");
     sign
       .update(
-        transaction.fromAddress + 
-        transaction.toAddress + 
-        transaction.value + 
-        transaction.fee
+        transaction.fromAddress +
+          transaction.toAddress +
+          transaction.value +
+          transaction.fee
       )
       .end();
     transaction.signature = sign.sign(this.privateKey, "hex");
   }
 
+  // Method to verify the signature of a transaction using the public key
   verifyTransaction(transaction) {
     const verify = crypto.createVerify("SHA256");
     verify
       .update(
         transaction.fromAddress +
-        transaction.toAddress +
-        transaction.value +
-        transaction.fee
+          transaction.toAddress +
+          transaction.value +
+          transaction.fee
       )
       .end();
     return verify.verify(this.publicKey, transaction.signature, "hex");
