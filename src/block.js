31f2cb804c926677e58b831fb842facb98eaf6d4
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-12-12 22:48:49 -0300

    docs: add comments in block class functions

--- src/block.js
+++ src/block.js
@@ -4,12 +4,15 @@ class Block {
   constructor(timestamp, lastHash, data, balanceBook) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
-    this.balanceBook = Array.from(balanceBook.entries()).sort(([a], [b]) => a.localeCompare(b));
+    this.balanceBook = Array.from(balanceBook.entries()).sort(([a], [b]) =>
+      a.localeCompare(b)
+    );
     this.data = data;
     this.nonce = 0;
     this.hash = this.calculateBlockHash();
   }
 
+  // Method to calculate the hash of the block
   calculateBlockHash() {
     return crypto
       .createHash("sha256")
@@ -23,11 +26,13 @@ class Block {
       .digest("hex");
   }
 
+  // Method to format the timestamp into a readable ISO string format
   formatTimestamp() {
     const date = new Date(this.timestamp);
     return date.toISOString();
   }
 
+  // Method to mine the block by solving the proof-of-work
   mineBlock(difficulty) {
     while (this.hash.substring(0, difficulty) !== "0".repeat(difficulty)) {
       this.nonce++;
