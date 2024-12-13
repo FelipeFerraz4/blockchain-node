624892132be274ea567443a513ee691e192bb58a
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-12-12 22:48:18 -0300

    docs: add comments in transaction class functions

--- src/transactions.js
+++ src/transactions.js
@@ -7,21 +7,35 @@ class Transactions {
     this.toAddress = toAddress;
     this.value = value;
     this.signature = null;
-    this.fee = fee
+    this.fee = fee;
   }
 
+  // Method to sign the transaction with the sender's private key
   signTransaction(privateKey) {
     const sign = crypto.createSign("SHA256");
     sign
-      .update(this.timestamp + this.fromAddress + this.toAddress + this.value + this.fee)
+      .update(
+        this.timestamp +
+          this.fromAddress +
+          this.toAddress +
+          this.value +
+          this.fee
+      )
       .end();
     this.signature = sign.sign(privateKey, "hex");
   }
 
+  // Method to verify the transaction's signature using the sender's public key
   verifyTransaction(publicKey) {
     const verify = crypto.createVerify("SHA256");
     verify
-      .update(this.timestamp + this.fromAddress + this.toAddress + this.value + this.fee)
+      .update(
+        this.timestamp +
+          this.fromAddress +
+          this.toAddress +
+          this.value +
+          this.fee
+      )
       .end();
     return verify.verify(publicKey, this.signature, "hex");
   }
